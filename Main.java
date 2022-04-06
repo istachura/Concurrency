933a94d4014f13b519c8fc0b0d19ae28562376e9
Iuliana <istachura@mail.valenciacollege.edu>
2022-04-06 14:24:54 -0400

    Added new files

---
+++ Main.java
@@ -0,0 +1,15 @@
+package sumArray;
+
+public class Main {
+
+	int[] randomNumber = new int [200000000];
+	int sum=0;
+	{
+	
+	for (int i = 0; i < 10; i++)
+	{	
+		sum=sum+randomNumber[i];
+	
+}
+}
+}
---
+++ MyConcurrency.java
@@ -0,0 +1,41 @@
+package sumArray;
+
+class Concurrency extends Thread{
+
+	@Override
+	public void run() {
+		
+		int[] randomNumber = new int [200000000];
+		
+		for (int i = 0; i < 10; i++)
+		{
+			randomNumber[i]= (int) (Math.random()*200000000);
+		}
+		for (int i = 0; i < 10; i++)
+		{
+	         System.out.println( Thread.currentThread().getId() + " Value " + randomNumber[i]);         
+	    }
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	}
+	
+
+
+public class MyConcurrency {
+
+	public static void main(String[] args) {
+		Concurrency thread1 = new Concurrency();
+		thread1.start();
+		Concurrency thread2 = new Concurrency();
+		thread2.start();
+		
+	}
+
+}
+	
